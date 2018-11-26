fe8eb96332025523d4baac6c348e5891897d1ad7
Vlad <vladhadarean7@gmail.com>
2018-11-26 16:47:16 +0000

    implemmented functionality

--- cwfiles/ot.py
+++ cwfiles/ot.py
@@ -3,7 +3,8 @@
 # naranker dulay, dept of computing, imperial college, october 2018
 from util import PrimeGroup, ot_hash, xor_bytes
 
-def ot_alice(self,socket, m0, m1):
+def ot_alice(socket, m0, m1):
+
     group = PrimeGroup()
     socket.send(group)
     socket.receive()
@@ -11,40 +12,47 @@ def ot_alice(self,socket, m0, m1):
     c = group.rand_int()
     socket.send(c)
     socket.receive()
+    socket.send('ok')
     h0 = socket.receive()
     socket.send('h0 received')
+    socket.receive()
 
     h1 = group.mul(c, group.inv(h0))
     k = group.rand_int()
     c1 = group.gen_pow(k)
 
-    e0 = xor_bytes(ot_hash(h0, len(m0)), str(m0).encode('UTF-8') )
-    e1 = xor_bytes(ot_hash(h1, len(m1)), str(m1).encode('UTF-8') )
+    bm0 = str(m0).encode('UTF-8')
+    bm1 = str(m1).encode('UTF-8')
 
-    socket.send((e0,e1,c1))
+    e0 = xor_bytes(ot_hash(group.pow(h0, k), len(bm0)), bm0)
+    e1 = xor_bytes(ot_hash(group.pow(h1, k), len(bm1)), bm1)
+
+    socket.send((e0,e1,c1,len(bm0)))
     socket.receive()
 
-def ot_bob(self, socket, bit):
+def ot_bob(socket, bit):
     group = socket.receive()
     socket.send('group received')
 
     c = socket.receive()
     socket.send('c received')
+    socket.receive()
     x = group.rand_int()
     hb = group.gen_pow(x)
-    socket.send(hb)
+    hb_inv = group.mul(c, group.inv(hb))
+    if bool(int(bit)):
+        socket.send(hb_inv)
+    else:
+        socket.send(hb)
     socket.receive()
-    (e0, e1, c1) = socket.receive()
+    socket.send('ok')
+    (e0, e1, c1, l) = socket.receive()
     socket.send('e0, e1, c1 received')
 
-    decrypt_hash = ot_hash(group.pow(c1,x))
-    if bool(bit):
+    decrypt_hash = ot_hash(group.pow(c1,x), l)
+    if bool(int(bit)):
         return eval(xor_bytes(e1, decrypt_hash).decode('UTF-8'))
     else:
         return eval(xor_bytes(e0, decrypt_hash).decode('UTF-8'))
-
-
-
-
 
 
